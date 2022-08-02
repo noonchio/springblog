e932ed4c8c5a5a3a450b8eb8c86a195e7eaddf5e
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-08-02 15:37:21 -0500

    add EmailService.java

--- src/main/java/com/example/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/example/codeup/springblog/controllers/PostController.java
@@ -6,6 +6,7 @@ import com.example.codeup.springblog.model.User;
 import com.example.codeup.springblog.reposotories.PostImageRepository;
 import com.example.codeup.springblog.reposotories.PostRepository;
 import com.example.codeup.springblog.reposotories.UserRepository;
+import com.example.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -25,11 +26,13 @@ public class PostController {
  private   UserRepository userDao;
 
  private PostImageRepository postImageDao;
+ private final EmailService emailService;
 
-    public PostController(PostRepository postDao, UserRepository userDao, PostImageRepository postImageDao) {
+    public PostController(PostRepository postDao, UserRepository userDao, PostImageRepository postImageDao, EmailService emailService) {
         this.postDao = postDao;
         this.userDao = userDao;
         this.postImageDao = postImageDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
@@ -49,7 +52,8 @@ public class PostController {
     }
 
     @GetMapping("/posts/create")
-    public String create() {
+    public String create(Model model) {;
+        model.addAttribute("newPost", new Post());
         return "posts/create";
     }
 
@@ -61,20 +65,21 @@ public class PostController {
 //        return "redirect:/posts";
 //    }
 //    delete a record with JPA
-    @GetMapping("/post/delete/{id}")
-    public String deletePost(@PathVariable long id) {
-        postDao.deleteById(id);
+    @GetMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable long id, Model model) {
+        Post post = postDao.findById(id).get();
+        postDao.delete(post);
         return "redirect:/posts";
     }
 
     @PostMapping( "/posts/create")
-    public String newPostandUser(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body, Model vModel) {
+    public String newPostandUser(@ModelAttribute Post post) {
         User user = userDao.findById(1L).get();
-        Post post = new Post(title, body);
         post.setUser(user);
         postDao.save(post);
 
 //       save works as update and create
+        emailService.prepareAndSend(post, "You creatred a new post" );
         return "redirect:/posts";
     }
 
@@ -85,6 +90,26 @@ public class PostController {
         vModel.addAttribute("posts", posttList);
         return "posts/showcats";
     }
+
+    @GetMapping("/posts/{id}/edit")
+    public String showUpdateForm(@PathVariable long id, Model model) {
+        Post post = postDao.findById(id).get();
+
+        model.addAttribute("post", post);
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(@PathVariable long id, @ModelAttribute Post post) {
+//         postDao.findById(id).get();
+        User user = userDao.findById(1L).get();
+        post.setUser(user);
+        postDao.save(post);
+        return "redirect:/posts" ;
+    }
+
+
+
 
 
 
